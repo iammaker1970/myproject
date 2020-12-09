7339b36828e16ac185f37669bdfe27f68c9888c6
kowine <kowine70@gmail.com>
2020-12-10 04:51:01 +0900

    20201210

--- machine_project/esp32_camera_mjpeg_multiclient/esp32_camera_mjpeg_multiclient.ino
+++ machine_project/esp32_camera_mjpeg_multiclient/esp32_camera_mjpeg_multiclient.ino
@@ -36,10 +36,10 @@
 
 // Select camera model
 //#define CAMERA_MODEL_WROVER_KIT
-#define CAMERA_MODEL_ESP_EYE
+//#define CAMERA_MODEL_ESP_EYE
 //#define CAMERA_MODEL_M5STACK_PSRAM
 //#define CAMERA_MODEL_M5STACK_WIDE
-//#define CAMERA_MODEL_AI_THINKER
+#define CAMERA_MODEL_AI_THINKER
 
 #include "camera_pins.h"
 
@@ -418,7 +418,7 @@ void setup()
   //  config.frame_size = FRAMESIZE_SVGA;
   //  config.frame_size = FRAMESIZE_QVGA;
   config.frame_size = FRAMESIZE_VGA;
-  config.jpeg_quality = 12;
+  config.jpeg_quality = 15;
   config.fb_count = 2;
 
 #if defined(CAMERA_MODEL_ESP_EYE)
